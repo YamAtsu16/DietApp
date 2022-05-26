bf5037ffccaa428e6ef852a31c3ed6a4c0ccb6cf
YamAtsu16 <00kisaragi16@gmail.com>
2022-05-26 23:03:12 +0900

    total_dataの取得と表示

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,9 @@
 class MealsController < ApplicationController
+
   def index
     @record = Record.find_by(start_time: Date.today)
     @meals = Meal.all
+    get_total_data
   end
 
   def new
@@ -25,4 +27,12 @@ class MealsController < ApplicationController
   def meal_params
     params.require(:meal).permit(:category_id, :eat_time_id, :food, :calorie, :protein, :fat, :carbo, :volume).merge(user_id: current_user.id, record_id: @record.id)
   end
+
+  def get_total_data
+    @total_calorie = @record.total_calorie
+    @total_protein = @record.total_protein
+    @total_fat = @record.total_fat
+    @total_carbo = @record.total_carbo
+  end
+
 end
