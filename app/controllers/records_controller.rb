a6e6d8d678d1ee4a67bfae6101e0a2a10b001181
YamAtsu16 <00kisaragi16@gmail.com>
2022-05-26 21:45:36 +0900

    record新規投稿

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,7 +5,22 @@ class RecordsController < ApplicationController
   def index
   end
 
+  def new
+    @record = Record.new
+    @today = Date.today
+  end
+
+  def create
+    @record = Record.new(record_params)
+    @record.save
+    redirect_to root_path
+  end
+
   private
+
+  def record_params
+    params.require(:record).permit(:start_time, :total_calorie, :total_protein, :total_fat, :total_carbo).merge(user_id: current_user.id)
+  end
 
   def set_beginning_of_week
     Date.beginning_of_week = :sunday
