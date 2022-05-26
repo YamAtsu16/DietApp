ec0e5c6a92807124c61d52fa4ad34136e5570c49
YamAtsu16 <00kisaragi16@gmail.com>
2022-05-26 21:30:49 +0900

    アソシエーションとバリデーション

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,12 @@
 class Meal < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :eat_time
+
+  belongs_to :user
+  belongs_to :record
+
+  validates :category_id, :eat_time_id, :food, :volume, :calorie, :protein, :fat, :carbo, presence: true
+  validates :calorie, :protein, :fat, :carbo, :volume, format: {with: /\A[0-9]+\z/}
+  validates :category_id, :eat_time_id, numericality: {other_than: 1}
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,6 @@
 class Record < ApplicationRecord
+  has_many :meals
+  belongs_to :user
+
+  validates :start_time, presence: true
 end
