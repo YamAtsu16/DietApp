acae8dc8eed3f639b6cbc7602760f35090afab0f
YamAtsu16 <00kisaragi16@gmail.com>
2022-05-28 12:08:40 +0900

    食事記録の合計値保存と表示

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,6 +3,7 @@ class MealsController < ApplicationController
   def create
     @meal = Meal.new(meal_params)
     if @meal.save
+      add_data
       redirect_to record_path(@meal.record_date_id)
     else
       @record = Record.find(params[:record_id])
@@ -14,6 +15,16 @@ class MealsController < ApplicationController
 
   def meal_params
     params.require(:meal).permit(:category_id, :eat_time_id, :food, :calorie, :protein, :fat, :carbo, :volume).merge(user_id: current_user.id, record_date_id: params[:record_id])
+  end
+
+  def add_data
+    record = Record.find(params[:record_id])
+
+    record.total_calorie += @meal.calorie
+    record.total_protein += @meal.protein
+    record.total_fat += @meal.fat
+    record.total_carbo += @meal.carbo
+    record.save
   end
 
 end
