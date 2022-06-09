d9d494738cbe3d8c67de1475c978a1b13139b377
YamAtsu16 <00kisaragi16@gmail.com>
2022-06-09 23:11:23 +0900

    body新規投稿

--- app/controllers/bodies_controller.rb
+++ app/controllers/bodies_controller.rb
@@ -1,2 +1,25 @@
 class BodiesController < ApplicationController
+  def index
+    @bodies = Body.all
+  end
+
+  def new
+    @body = Body.new
+  end
+
+  def create
+    body = Body.new(body_params)
+    if body.save
+      redirect_to bodies_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def body_params
+    params.require(:body).permit(:weight, :image, :memo).merge(user_id: current_user.id)
+  end
+
 end
