2069d2efb864c113b27c6cb324ccd1e59c2e9d7f
YamAtsu16 <00kisaragi16@gmail.com>
2022-06-09 22:44:13 +0900

    bodiesコントローラー及びbodyモデルの作成

---
+++ db/migrate/20220609133258_create_bodies.rb
@@ -0,0 +1,10 @@
+class CreateBodies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bodies do |t|
+      t.integer :weight, null: false
+      t.text :memo
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_121956) do
+ActiveRecord::Schema.define(version: 2022_06_09_133258) do
+
+  create_table "bodies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "weight", null: false
+    t.text "memo"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bodies_on_user_id"
+  end
 
   create_table "meals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "category_id", null: false
@@ -53,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_05_26_121956) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bodies", "users"
   add_foreign_key "meals", "records"
   add_foreign_key "meals", "users"
   add_foreign_key "records", "users"
