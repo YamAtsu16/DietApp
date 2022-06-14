5b126db74597724088b03d726ee4e423de86317e
YamAtsu16 <00kisaragi16@gmail.com>
2022-06-14 18:35:07 +0900

    recipeモデルの作成

--- db/migrate/20220609133258_create_bodies.rb
+++ db/migrate/20220609133258_create_bodies.rb
@@ -2,7 +2,7 @@ class CreateBodies < ActiveRecord::Migration[6.0]
   def change
     create_table :bodies do |t|
       t.date :start_time, null: false
-      t.ffloat :weight, null: false
+      t.float :weight, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
