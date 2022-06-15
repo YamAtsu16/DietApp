15a337a004d72943d5e151916c59ac022afd1d30
YamAtsu16 <00kisaragi16@gmail.com>
2022-06-15 10:27:13 +0900

    マイレシピ新規投稿

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,9 +3,16 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -17,6 +24,13 @@ class RecipesController < ApplicationController
   def update
   end
 
-  def delete
+  def destroy
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:recipe_name, :text, :calorie, :protein, :fat, :carbo).merge(user_id: current_user.id)
+  end
+
 end
