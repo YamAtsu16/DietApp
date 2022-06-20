50c1887410234f5e113e1207416603366658cbe2
YamAtsu16 <00kisaragi16@gmail.com>
2022-06-20 21:49:22 +0900

    recipe単体モデルテスト

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :recipe do
-    
+    recipe_name { Faker::Name.name }
+    text        { Faker::Lorem.sentence }
+    calorie     { Faker::Number.between(from: 0, to: 999) }
+    protein     { Faker::Number.between(from: 0, to: 999) }
+    fat         { Faker::Number.between(from: 0, to: 999) }
+    carbo       { Faker::Number.between(from: 0, to: 999) }
+    association :user
+
+    after(:build) do |recipe|
+      recipe.image.attach(io: File.open('public/images/food.png'), filename: 'food.png')
+    end
   end
 end
